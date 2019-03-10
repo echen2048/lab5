2c60736d79ca499ad2b0884d6065e6bdb1e4e9f4
echen <eric.c@utexas.edu>
2019-03-10 15:33:30 -0500

    added gpio definitions

--- StepperMotorController.c
+++ StepperMotorController.c
@@ -17,6 +17,24 @@
 #include <stdint.h>
 #include "../inc/tm4c123gh6pm.h"
 
+#define SYSCTL_RCGCGPIO_R       (*((volatile uint32_t *)0x400FE608))
+
+#define GPIO_PORTA_DATA_R				(*((volatile uint32_t *)0x400043FC))
+#define GPIO_PORTA_DIR_R				(*((volatile uint32_t *)0x40004400))
+#define GPIO_PORTA_DEN_R				(*((volatile uint32_t *)0x4000451C))
+
+#define GPIO_PORTB_DATA_R       (*((volatile uint32_t *)0x400053FC))
+#define GPIO_PORTB_DIR_R        (*((volatile uint32_t *)0x40005400))
+#define GPIO_PORTB_DEN_R        (*((volatile uint32_t *)0x4000551C))
+
+#define GPIO_PORTE_DATA_R       (*((volatile uint32_t *)0x400243FC))
+#define GPIO_PORTE_DIR_R        (*((volatile uint32_t *)0x40024400))
+#define GPIO_PORTE_DEN_R        (*((volatile uint32_t *)0x4002451C))
+
+#define GPIO_PORTF_DATA_R				(*((volatile uint32_t *)0x400253FC))
+#define GPIO_PORTF_DIR_R				(*((volatile uint32_t *)0x40025400))
+#define GPIO_PORTF_DEN_R 				(*((volatile uint32_t *)0x4002551C))
+
 void EnableInterrupts(void);
 // edit the following only if you need to move pins from PA4, PE3-0      
 // logic analyzer on the real board
@@ -26,13 +44,24 @@ void SendDataToLogicAnalyzer(void){
   UART0_DR_R = 0x80|(PA4<<2)|PE50;
 }
 
+//write functions for spin motor with led flash input arg
+
 int main(void){ 
   TExaS_Init(&SendDataToLogicAnalyzer);    // activate logic analyzer and set system clock to 80 MHz
   SysTick_Init();   
 // you initialize your system here
 	
+	//set up statemachine and inputs
+
+	
   EnableInterrupts();   
   while(1){
+		//check current state output
+		//conditional if
+		//if output is 1, fcnx
+		//if output is 2, fcny
+		//if output is 0, check current state again
+		
 // output
 // wait
 // input
